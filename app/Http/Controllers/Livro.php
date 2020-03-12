86b1786d3ed2f43324aadcee58705087e0adebb9
FabioNog22 <desenvolvimento22@soitic.com>
2020-03-12 10:08:53 -0300

    Create function livros

--- app/Http/Controllers/Livro.php
+++ app/Http/Controllers/Livro.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class Livro extends Controller
 {
-    //
+    public function livros(){
+        echo "<h1>Livros</h1>";
+        echo "<li>Dom Casmurro </li>";
+        echo "<li>A Barça </li>";        
+        echo "<li>Meu pé de laranja lima </li>";
+        echo "</ol>";
+    }
 }
