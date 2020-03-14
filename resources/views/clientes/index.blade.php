43e61756546aad7522d909bafe929ad110277526
FabioNog22 <desenvolvimento22@soitic.com>
2020-03-14 10:17:29 -0300

    Alter view index for link with my create view and edit view

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -1,7 +1,9 @@
 <h3>Bem vindo aos Clientes</h3>
 
+<a href="{{route('clientes.create')}}">Novo Usuario</a>
+
 <ol>
     @foreach ($clientes as $c)
-        <li>{{$c['cliente_nome']}}</li>
+        <li>{{$c['cliente_nome']}} | <a href="{{route('clientes.edit',$c['id'])}}">Editar</a></li> 
     @endforeach
 </ol>
